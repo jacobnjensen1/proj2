d7df74be21baeafb69ed02f079eca8df99619e09
Jacob Jensen <jnjensen88@gmail.com>
2020-10-17 17:43:44 -0600

    finished

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const endURL = "?page=1&limit=100"
 
 getRandomQuote() //Just to get something at the beginning
 
-let currentGenre = "uninitialized"  //will get changed with each response
-let currentAuthor = "A Person"      //will change each time
+let currentGenre = "uninitialized" //will get changed with each response
+let currentAuthor = "A Person" //will change each time
 
 document.getElementById("randomButton").addEventListener("click", function (event) {
     event.preventDefault()
@@ -25,36 +25,36 @@ document.getElementById("authorButton").addEventListener("click", function (even
 
 function getRandomQuote() {
     fetch(randomURL)
-    .then(function (response) {
-        return response.json()
-    }).then(function (json) {
-        console.log(json)
-        handleResonse(json.quote)
-    })
+        .then(function (response) {
+            return response.json()
+        }).then(function (json) {
+            console.log(json)
+            handleResonse(json.quote)
+        })
 }
 
 function getGenreQuote() {
     let url = genreBaseURL + currentGenre + endURL
     fetch(url)
-    .then(function (response) {
-        return response.json()
-    }).then(function (json) {
-        console.log(json)
-        let index = Math.floor(Math.random() * json.quotes.length)
-        handleResonse(json.quotes[index])
-    })
+        .then(function (response) {
+            return response.json()
+        }).then(function (json) {
+            console.log(json)
+            let index = Math.floor(Math.random() * json.quotes.length)
+            handleResonse(json.quotes[index])
+        })
 }
 
 function getAuthorQuote() {
     let url = authorBaseURL + currentAuthor + endURL
     fetch(url)
-    .then(function (response) {
-        return response.json()
-    }).then(function (json) {
-        console.log(json)
-        let index = Math.floor(Math.random() * json.quotes.length)
-        handleResonse(json.quotes[index])
-    })
+        .then(function (response) {
+            return response.json()
+        }).then(function (json) {
+            console.log(json)
+            let index = Math.floor(Math.random() * json.quotes.length)
+            handleResonse(json.quotes[index])
+        })
 }
 
 function handleResonse(json) {
